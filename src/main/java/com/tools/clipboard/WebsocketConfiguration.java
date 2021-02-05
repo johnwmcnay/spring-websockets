afdcc28eeaf2bf9365abb8a1f2b9e5292955f6a3
johnwmcnay <john.w.mcnay@gmail.com>
2021-02-05 11:54:06 -0600

    updated config in attempt to fix production issues

--- src/main/java/com/tools/clipboard/WebsocketConfiguration.java
+++ src/main/java/com/tools/clipboard/WebsocketConfiguration.java
@@ -1,12 +1,14 @@
 package com.tools.clipboard;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
+@Import(RedisConfiguration.class)
 @EnableWebSocketMessageBroker
 public class WebsocketConfiguration implements WebSocketMessageBrokerConfigurer {
 
