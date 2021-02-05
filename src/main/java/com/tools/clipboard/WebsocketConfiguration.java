0402d6310da09b8e967ef3dabbd36f51443ca5e4
johnwmcnay <john.w.mcnay@gmail.com>
2021-02-05 15:27:43 -0600

    still troubleshooting

--- src/main/java/com/tools/clipboard/WebsocketConfiguration.java
+++ src/main/java/com/tools/clipboard/WebsocketConfiguration.java
@@ -7,8 +7,8 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@Configuration
-@EnableWebSocketMessageBroker
+
+@EnableWebSocketMessageBroker @Configuration
 public class WebsocketConfiguration implements WebSocketMessageBrokerConfigurer {
 
     @Override
