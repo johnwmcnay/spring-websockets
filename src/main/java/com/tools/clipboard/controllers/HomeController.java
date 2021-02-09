07993f4f3b1124351fad643b9c58d1bcaa354ce9
johnwmcnay <john.w.mcnay@gmail.com>
2021-02-09 11:29:47 -0600

    trying out connect in java backend

--- src/main/java/com/tools/clipboard/controllers/HomeController.java
+++ src/main/java/com/tools/clipboard/controllers/HomeController.java
@@ -3,6 +3,7 @@ package com.tools.clipboard.controllers;
 import com.tools.clipboard.models.MyClientEndpoint;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -32,6 +33,13 @@ public class HomeController {
      public String test() {
 
         return "test";
+    }
+
+    @PostMapping("/connect")
+    @ResponseBody
+    public String connectTest() {
+        System.out.println("connectTest()");
+        return "connectTest()";
     }
 
 }
--- src/main/java/com/tools/clipboard/models/MyClientEndpoint.java
+++ src/main/java/com/tools/clipboard/models/MyClientEndpoint.java
@@ -1,6 +1,7 @@
 package com.tools.clipboard.models;
 
 import javax.websocket.ClientEndpoint;
+import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 
@@ -9,7 +10,16 @@ public class MyClientEndpoint {
 
     @OnOpen
     public void myOnOpen (Session session) {
+        System.out.println("session.getPathParameters() = " + session.getPathParameters());
+        System.out.println("session.getRequestParameterMap() = " + session.getRequestParameterMap());
+        System.out.println("session.getQueryString() = " + session.getQueryString());
         System.out.println ("WebSocket opened: "+session.getId());
+    }
+
+    @OnMessage
+    public String myOnMessage (String txt) {
+        System.out.println("txt = " + txt);
+        return txt;
     }
 
 }
