3f4b8b32bdf356ddd0639eacbd728df75e1ce847
johnwmcnay <john.w.mcnay@gmail.com>
2021-02-09 08:53:11 -0600

    testing endpoint with AWS websocket API

--- src/main/java/com/tools/clipboard/controllers/HomeController.java
+++ src/main/java/com/tools/clipboard/controllers/HomeController.java
@@ -2,6 +2,7 @@ package com.tools.clipboard.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -9,6 +10,12 @@ public class HomeController {
     @GetMapping("/")
     public String landingPage() {
         return "index";
+    }
+
+    @GetMapping("/testing")
+    @ResponseBody
+    public String test() {
+        return "test";
     }
 
 }
